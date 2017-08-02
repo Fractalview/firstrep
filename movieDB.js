c099cbee0f5433381ff52723779a28fbb3a18934
Miljenko Vujaklija <mvujaklija@circlewf.com>
2017-08-02 14:45:33 +0200

    Commented out code

--- movieDB.js
+++ movieDB.js
@@ -1,14 +1,14 @@
-movies = [ {title:"'Daisies'", stars:5, seen:true}, {title:"'Baraka'", stars:5, seen:true}, {title:"'Heathers'", stars:2, seen:true}, {title:"'Mad Max'", stars:4, seen:false}]
+// movies = [ {title:"'Daisies'", stars:5, seen:true}, {title:"'Baraka'", stars:5, seen:true}, {title:"'Heathers'", stars:2, seen:true}, {title:"'Mad Max'", stars:4, seen:false}]
 
-function movieDB (arr) {
-    for (var i = 0; i< arr.length; i++) {
-        if (arr[i].seen == true) { 
-            console.log("You have watched " + arr[i].title + " - " + arr[i].stars + " stars");
-        }
-        else {
-            console.log("You have not seen " + arr[i].title + " - " + arr[i].stars + " stars");
-        }
-    }
-}
+// function movieDB (arr) {
+//     for (var i = 0; i< arr.length; i++) {
+//         if (arr[i].seen == true) { 
+//             console.log("You have watched " + arr[i].title + " - " + arr[i].stars + " stars");
+//         }
+//         else {
+//             console.log("You have not seen " + arr[i].title + " - " + arr[i].stars + " stars");
+//         }
+//     }
+// }
 
-movieDB(movies);
+// movieDB(movies);
